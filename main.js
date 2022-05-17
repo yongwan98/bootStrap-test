a6763cfc0eca5978e85fda1362d8884a9990e64b
yongwan98 <yongwan1998@gmail.com>
2022-05-17 12:03:43 +0900

    bootstrap end

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
-import bootstrap from 'bootstrap/dist/js/bootstrap.bundle'
+// import bootstrap from 'bootstrap/dist/js/bootstrap.bundle'
+// import Dropdown from 'bootstrap/js/dist/dropdown'
+import Modal from 'bootstrap/js/dist/modal'
+
+// 개별기능 modal
+new Modal('#exampleModal', {
+  backdrop: 'static'
+})
+
+// 개별기능 dropdown
+// const dropdownElementList = document.querySelectorAll('.dropdown-toggle');
+// [...dropdownElementList].map(dropdownToggleEl => new Dropdown(dropdownToggleEl))
 
 // modal
-const emailInputEl = document.querySelector('#exampleInputEmail1')
-const modalEl = document.querySelector('#exampleModal')
+// const emailInputEl = document.querySelector('#exampleInputEmail1')
+// const modalEl = document.querySelector('#exampleModal')
 
-modalEl.addEventListener('shown.bs.modal', function() {
-  emailInputEl.focus()
-})
+// modalEl.addEventListener('shown.bs.modal', function() {
+//   emailInputEl.focus()
+// })
 
 // toolTip
-let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+// let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+// let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+//   return new bootstrap.Tooltip(tooltipTriggerEl)
+// })
